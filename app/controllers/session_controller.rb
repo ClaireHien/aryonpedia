dc22dcc925a738c69b8c15b1c4a55daad82e3a76
ClaireHien <clairehien.osmont@gmail.com>
2020-06-18 00:02:03 +0200

    controller session

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,29 +6,24 @@ class SessionController < ApplicationController
 
   def create
 
-    @user = User.find_by(pseudo: params["u_pseudo"])
-    puts "#"*50
-    puts User.find_by(pseudo: params["u_pseudo"])
-    puts "#"*50
-    connexion = @user.authenticate(params["u_password"])
-    puts  @user.authenticate(params["u_password"])
-    puts "#"*50
+    user = User.find_by(pseudo: params["u_pseudo"])
+    connexion = user.authenticate(params["u_password"])
 
     if connexion == false
       puts "------------- connexion refusée"
       render "/session/new"
     else
       puts "------------- connexion effectuée"
-      session[:user_id] = @user.id
+      session[:user_id] = user.id
       redirect_to "/"
     end
   
   end
 
+
   def destroy
-    puts "hello!"
     user_id = current_user.id
-    session.delete(user_id)
+    session.destroy
     redirect_to "/"
   end
 
